dd3456f763f7104e14474b7816ead09131630f2d
Timothy Karani <kartimothy@gmail.com>
2021-12-26 23:08:35 +0300

    Added drf_yasg

--- backend/settings.py
+++ backend/settings.py
@@ -49,8 +49,8 @@ INSTALLED_APPS = [
     'allauth.socialaccount',
     'dj_rest_auth.registration',
 
-
     'corsheaders',
+    'drf_yasg',
 
     # local
     'msgs',
--- backend/urls.py
+++ backend/urls.py
@@ -15,6 +15,22 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include
+from rest_framework import permissions
+from drf_yasg import openapi
+from drf_yasg.views import get_schema_view
+
+schema_view = get_schema_view(
+    openapi.Info(
+        title="Antimony Chat API",
+        default_version="v1",
+        description="Antimony Chat backend logic",
+        terms_of_service="https://www.gnu.org/licenses/agpl-3.0.en.html",
+        contact=openapi.Contact(email="kartimothy@gmail.com"),
+        license=openapi.License(name="AGPL-3.0-or-later"),
+    ),
+    public=True,
+    permission_classes=(permissions.AllowAny,),
+)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
@@ -23,4 +39,10 @@ urlpatterns = [
     path('api-auth/', include('rest_framework.urls')),
     path('api/v1/auth/', include('dj_rest_auth.urls')),
     path('api/v1/auth/register/', include('dj_rest_auth.registration.urls')),
+
+    # docs
+    path('swagger/', schema_view.with_ui(
+        'swagger', cache_timeout=0), name="schema-swagger-ui"),
+    path('redoc/', schema_view.with_ui(
+        'redoc', cache_timeout=0), name="schema-redoc"),
 ]
