234b9208f6908f5832d6e432ee70ab7222b1050c
Timothy Karani <kartimothy@gmail.com>
2021-12-26 23:13:54 +0300

    Added messages models

--- msgs/models.py
+++ msgs/models.py
@@ -1,3 +1,9 @@
+from django.contrib.auth import get_user_model
 from django.db import models
 
-# Create your models here.
+
+class Msg(models.Model):
+    message = models.TextField()
+    user_id = models.OneToOneField(
+        get_user_model(), on_delete=models.PROTECT)
+    created_at = models.DateTimeField(auto_now_add=True)
