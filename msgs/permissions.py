a3687c093bb5b6bc84151109077d86694378412f
Timothy Karani <kartimothy@gmail.com>
2021-12-28 22:14:48 +0300

    Updated permissions

--- msgs/permissions.py
+++ msgs/permissions.py
@@ -3,4 +3,10 @@ from rest_framework import permissions
 
 class IsSenderOrRecepient(permissions.BasePermission):
     def has_object_permission(self, request, view, obj):
-        return obj.user_from == request.user or obj.user_to == request.user
+        # Read-only permissions for sender or receiver
+        is_sender_or_receiver = obj.user_from == request.user or obj.user_to == request.user
+        if is_sender_or_receiver and request.method in permissions.SAFE_METHODS:
+            return True
+
+        # Write permissions for sender only
+        return obj.user_from == request.user
