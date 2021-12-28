204a814db44a4afbe30e5993fe31f876a08b0132
Timothy Karani <kartimothy@gmail.com>
2021-12-28 22:54:39 +0300

    Showing messages for logged in user only

--- msgs/views.py
+++ msgs/views.py
@@ -10,8 +10,14 @@ from .serializers import MsgSerializer
 
 class MsgViewSet(FiltersMixin, viewsets.ModelViewSet):
     permission_classes = (permissions.IsAuthenticated, IsSenderOrRecepient,)
-    queryset = Msg.objects.all()
     serializer_class = MsgSerializer
+
+    # queryset = Msg.objects.all()
+    def get_queryset(self):
+        user = self.request.user
+        queryset = Msg.objects.filter(
+            user_from=user) | Msg.objects.filter(user_to=user)
+        return queryset.order_by('id')
 
     filter_backends = (filters.OrderingFilter,)
     filter_mappings = {
