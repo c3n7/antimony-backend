aeb4ceaec2d1ad8038c60cb9907c190f9045977c
Timothy Karani <kartimothy@gmail.com>
2022-02-01 02:14:43 +0300

    CSRF fix attempt 012

--- users/views.py
+++ users/views.py
@@ -1,8 +1,10 @@
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from django.middleware.csrf import get_token
+from django.views.decorators.csrf import ensure_csrf_cookie
 
 
+@ensure_csrf_cookie
 @api_view()
 def get_csrf_token(request):
     token = get_token(request)
