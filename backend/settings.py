1b3ca76d333fa0976c6cfe52c59e4941b7fdc878
Timothy Karani <kartimothy@gmail.com>
2022-01-31 22:21:40 +0300

    Allowing heroku subdomains

--- backend/settings.py
+++ backend/settings.py
@@ -186,6 +186,8 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True
 
+CSRF_COOKIE_DOMAIN = ".herokuapp.com"
+
 # rest_framework permission configs
 REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': [
