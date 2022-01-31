be1fb3a3670fcd031faa4547ac50038d84dc8bec
Timothy Karani <kartimothy@gmail.com>
2022-02-01 01:55:34 +0300

    JSON result

--- users/views.py
+++ users/views.py
@@ -6,4 +6,4 @@ from django.middleware.csrf import get_token
 @api_view()
 def get_csrf_token(request):
 
-    return Response({'csrfToken', get_token(request)})
+    return Response({'csrfToken': get_token(request)})
