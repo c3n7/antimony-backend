fe1ede2c6b89d4e4e4b1a0fffb75e9cb9ccd7b7a
Timothy Karani <kartimothy@gmail.com>
2022-02-01 00:48:08 +0300

    CSRF fix attempt 004

--- backend/settings.py
+++ backend/settings.py
@@ -187,7 +187,7 @@ SESSION_COOKIE_SECURE = True
 CSRF_COOKIE_SAMESITE = 'None'
 CSRF_COOKIE_SECURE = True
 
-CSRF_COOKIE_DOMAIN = ".herokuapp.com"
+CSRF_COOKIE_DOMAIN = "herokuapp.com"
 
 # rest_framework permission configs
 REST_FRAMEWORK = {
