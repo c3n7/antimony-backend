c33ba3094a597e69c2b1afbf4d0cecac790a4df8
Timothy Karani <kartimothy@gmail.com>
2022-02-01 09:13:59 +0300

    Using token authentication

--- backend/settings.py
+++ backend/settings.py
@@ -198,8 +198,8 @@ REST_FRAMEWORK = {
         'rest_framework.permissions.AllowAny'
     ],
     'DEFAULT_AUTHENTICATION_CLASSES': [
-        'rest_framework.authentication.SessionAuthentication',
         'rest_framework.authentication.TokenAuthentication',
+        # 'rest_framework.authentication.SessionAuthentication',
     ]
 }
 
